868128dac5c33c83391201a5dc354541f4be9d43
KATA <95535831+K-A-T-A@users.noreply.github.com>
2022-11-01 13:57:51 +0400

    Add files via upload

---
+++ web2-main/js/script.js
@@ -0,0 +1,13 @@
+$('.review-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
+
+sliderSecondBlock()
+function sliderSecondBlock(){
+    $('.reviews .reviewsBlock .info-block .item').css('opacity', 0.3);
+    $('.reviews .reviewsBlock .info-block .item.slick-active').eq(1).css('opacity', 1);
+
+}
+
